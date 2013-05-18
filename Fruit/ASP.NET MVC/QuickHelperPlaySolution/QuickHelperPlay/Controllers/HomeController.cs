f66a0728a1c35a53c0f3145248cd8417c5b5f739
Anubhav Saini <iamanubhavsaini@gmail.com>
2013-05-18 15:32:25 +0530

    ASP.NET MVC Helper example "QuickHelperSolution" added
    Signed-off-by: Anubhav Saini <iamanubhavsaini@gmail.com>

---
+++ Fruit/ASP.NET MVC/QuickHelperPlaySolution/QuickHelperPlay/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace QuickHelperPlay.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            ViewBag.R1 = new List<string> { "one" };
+            ViewBag.R2 = new List<string>();
+            ViewBag.R3 = new List<string> { "one", "two", "three" };
+            return View();
+        }
+
+    }
+}
---
+++ Fruit/ASP.NET MVC/QuickHelperPlaySolution/QuickHelperPlay/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="QuickHelperPlay.MvcApplication" Language="C#" %>
